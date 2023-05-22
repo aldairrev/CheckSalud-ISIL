460a6684e1210b32bc6a1135fec2b4d8f4d00865
Rodrigoloq <rodcxd1234@gmail.com>
2023-05-21 22:19:02 -0500

    metodos de serviciomedico

--- WCF_CheckSalud/ServicioMedico.cs
+++ WCF_CheckSalud/ServicioMedico.cs
@@ -11,24 +11,69 @@ namespace WCF_CheckSalud
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioMedico" en el código y en el archivo de configuración a la vez.
     public class ServicioMedico : IServicioMedico
     {
-        public bool ActualizarMedico(MedicoDC medico)
+        public bool ActualizarMedico(MedicoDC objMedicoDC)
         {
             try
             {
+                //declaro instancia del modelo BD
                 CheckSaludEntities MisCkSalud = new CheckSaludEntities();
 
-                MisCkSalud.usp_ActualizarMedico();
+                MisCkSalud.usp_ActualizarMedico(objMedicoDC.codMedico, objMedicoDC.dniMedic,
+                    objMedicoDC.nomMedico, objMedicoDC.apeMedico, objMedicoDC.numColegiatura,
+                    objMedicoDC.fechaNacimiento, objMedicoDC.emailMedico, objMedicoDC.direcMedico,
+                    objMedicoDC.estado, objMedicoDC.codEspecialidad, objMedicoDC.Login_Usuario,
+                    objMedicoDC.Usu_Ult_Mod, objMedicoDC.Id_Ubigeo);
+                MisCkSalud.SaveChanges();
+                return true;
             }
-            catch (Exception)
+            catch (EntityException ex)
             {
-
-                throw;
+                throw new Exception(ex.Message);
+                return false;
             }
         }
 
         public MedicoDC ConsultarMedico(string strCodigo)
         {
-            throw new NotImplementedException();
+            CheckSaludEntities MisCkSalud = new CheckSaludEntities();
+
+            try
+            {
+                Tb_Medico objConsulta = (from misCkSalud in MisCkSalud.Tb_Medico
+                                        where misCkSalud.codMedico == strCodigo
+                                        select misCkSalud).FirstOrDefault();
+
+                MedicoDC objMedicoDC = new MedicoDC();
+
+                if(objConsulta != null)
+                {
+                    objMedicoDC.codMedico = "";
+                }
+                objMedicoDC.codMedico = objConsulta.codMedico;
+                objMedicoDC.dniMedic = objConsulta.dniMedic;
+                objMedicoDC.nomMedico = objConsulta.nomMedico;
+                objMedicoDC.apeMedico = objConsulta.apeMedico;
+                objMedicoDC.apellNomMedico = objConsulta.nomMedico + ", " + objConsulta.apeMedico;
+                objMedicoDC.numColegiatura = objConsulta.numColegiatura;
+                objMedicoDC.fechaNacimiento = objConsulta.fechaNacimiento;
+                objMedicoDC.emailMedico = objConsulta.emailMedico;
+                objMedicoDC.direcMedico = objConsulta.direcMedico;
+                objMedicoDC.estado = Convert.ToInt16(objConsulta.estado);
+                objMedicoDC.codEspecialidad = objConsulta.codEspecialidad;
+                objMedicoDC.Login_Usuario = objConsulta.Login_Usuario;
+                objMedicoDC.Fec_Reg = Convert.ToDateTime(objConsulta.Fec_Reg);
+                objMedicoDC.Usu_Registro = objConsulta.Usu_Registro;
+                objMedicoDC.Fech_Ult_Mod = Convert.ToDateTime(objConsulta.Fech_Ult_Mod);
+                objMedicoDC.Usu_Ult_Mod = objConsulta.Usu_Ult_Mod;
+                objMedicoDC.Id_Ubigeo = objConsulta.Id_Ubigeo;
+                objMedicoDC.fec_ingreso = Convert.ToDateTime(objConsulta.fec_ingreso);
+
+                return objMedicoDC;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public bool EliminarMedico(string strCodigo)
@@ -49,9 +94,26 @@ namespace WCF_CheckSalud
             }
         }
 
-        public bool InsertarMedico(MedicoDC medico)
+        public bool InsertarMedico(MedicoDC objMedicoDC)
         {
-            throw new NotImplementedException();
+            try
+            {
+                //declaro instancia del modelo BD
+                CheckSaludEntities MisCkSalud = new CheckSaludEntities();
+
+                MisCkSalud.usp_InsertarMedico(objMedicoDC.dniMedic,
+                    objMedicoDC.nomMedico, objMedicoDC.apeMedico, objMedicoDC.numColegiatura,
+                    objMedicoDC.fechaNacimiento, objMedicoDC.emailMedico, objMedicoDC.direcMedico,
+                    objMedicoDC.estado, objMedicoDC.codEspecialidad, objMedicoDC.Login_Usuario,
+                    objMedicoDC.Usu_Registro, objMedicoDC.Id_Ubigeo, objMedicoDC.fec_ingreso);
+                MisCkSalud.SaveChanges();
+                return true;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+                return false;
+            }
         }
 
         public List<MedicoDC> ListarMedicos()
